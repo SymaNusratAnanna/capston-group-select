71cccf15dc88c6d5de16bdf9138ee00cd0a565d7
SymaNusratAnanna <symanusrat158@gmail.com>
2022-03-26 17:31:53 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import Header from './components/Header/Header';
+import Student from './components/Students/Student';
+
+function App() {
+  return (
+    <div>
+      <Header></Header>
+      <Student></Student>
+    </div>
+  );
+}
+
+
+export default App;
---
+++ src/components/Header/Header.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Header.css';
+
+const Header = () => {
+    return (
+        <div className="header">
+            <h2 className="header-title">Select your best Capston projects group</h2>
+            <p>In the system of BUBT engineering department, to achive the graduation from any engineering departments a someone must need to submit ther capston projects. Capston project is a final year project for CSE student.It is a combine course for thesis and project together.</p>
+            <h3>Total Weiver Budget for each group: 50000</h3>
+        </div>
+    );
+};
+
+export default Header;
